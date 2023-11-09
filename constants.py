9a79097023085bba76ae5f4d2c12d153e2c11dcd
misto <ottonellicopattibruno@gmail.com>
2023-11-09 12:13:28 -0300

    Add TODO macos path

--- constants.py
+++ constants.py
@@ -6,6 +6,7 @@ if sys.platform == "linux":
     LOCKFILEPATH = f"/home/{os.getlogin()}/Games/league-of-legends/drive_c/Riot Games/League of Legends/lockfile"
 elif sys.platform == "win32":
     LOCKFILEPATH = "C:\Riot Games\League of Legends\lockfile"
+    # TODO: macos path
 
 HOST = "127.0.0.1"
 USERNAME = "riot"
