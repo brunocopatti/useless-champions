db3b7dc149bde8a63c071e0da2690d8d61cc9671
misto <ottonellicopattibruno@gmail.com>
2023-11-09 12:01:53 -0300

    Test more cases

--- test_project.py
+++ test_project.py
@@ -12,17 +12,20 @@ with open("test_champions_short.json") as file:
 def test_parse_champion_name():
     assert project.parse_champion_name(champions_large, "") == None
     assert project.parse_champion_name(champions_large, "briar") == "CHAMPION_RENTAL_233"
+    assert project.parse_champion_name(champions_large, "xeRAth") == "CHAMPION_RENTAL_101"
+    assert project.parse_champion_name(champions_large, "axe") == None
 
 
 def test_parse_champion_arguments():
     assert project.parse_champion_arguments(champions_large, []) == {}
-    assert project.parse_champion_arguments(champions_large, ["briar", 1, "xerath", 1]) == {
+    assert project.parse_champion_arguments(champions_large, ["briar", 1, "XERATH", 1]) == {
         "CHAMPION_RENTAL_233": 1,
         "CHAMPION_RENTAL_101": 1
     }
 
 
 def test_parse_champions():
+    assert project.parse_champions([]) == {}
     assert project.parse_champions(champions_short) == {
         "CHAMPION_RENTAL_104": 2,
         "CHAMPION_RENTAL_234": 1,
